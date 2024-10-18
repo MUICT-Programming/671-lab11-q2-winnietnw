5bcd0ad288b2cd8c6ddbb639d886f319347f8574
winnietnw <winniehswm@gmail.com>
2024-10-18 16:50:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # YOUR CODE HERE
+def summation(lst1,lst2,n):
+    update_list=[]
+    for i in range(n):
+        update_list.append([lst1[i]+lst2[i]])
+    return update_list
+def find_min_max(lst):
+    return (min(lst),max(lst))
+
+
+x=[]
+y=[]
+n=int(input())
+for i in range(n):
+    x.append(int(input()))
+for i in range(n):
+    y.append(int(input()))
+result= summation(x,y,n)
+print(result)
+minmax=find_min_max(result)
+print(minmax)
